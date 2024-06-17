5912faa34699c3be6a40c1d54c0326ae331db9d3
Justin Ferber <119906373+j-ferber@users.noreply.github.com>
2024-06-17 18:55:16 -0400

    fixed user searching retry (#2)

--- src/app/(protected)/search/page.tsx
+++ src/app/(protected)/search/page.tsx
@@ -23,47 +23,49 @@ const page = () => {
       </ResponsiveContainer>
     )
   }
-  const {data, isLoading, failureCount} = api.user.searchForUsers.useQuery({username: searchParams})
-  if (isLoading && failureCount < 1) return (
-    <ResponsiveContainer className="flex flex-col items-center justify-center gap-2">
-      <LoaderCircle className="animate-spin text-white h-16 w-16"/>
-    </ResponsiveContainer>
-  )
-  if (data === undefined) return (
-    <ResponsiveContainer className="flex flex-col items-center justify-center gap-8">
-      <h1 className="md:text-4xl text-2xl font-bold text-white">No users found.</h1>
-      <Link href={"/"} className={buttonVariants({variant: "secondary"})}>
-        Return Home
-      </Link>
-    </ResponsiveContainer>
-  )
+  const {data, isPending, isError} = api.user.searchForUsers.useQuery({username: searchParams}, {retry: false})
 
   return (
-    <ResponsiveContainer>
-      <div className="grid w-full gap-4 sm:grid-cols-2 grid-cols-1">
-        {
-          data.map((user) => (
-            <Card key={user.id}>
-              <CardHeader className="space-y-4">
-                <Avatar>
-                  <AvatarImage src={user.image ? user.image : "/avatar.png"} />
-                  <AvatarFallback>{user.username?.charAt(0).toUpperCase()}</AvatarFallback>
-                </Avatar>
-                <CardTitle>{user.username}</CardTitle>
-              </CardHeader>
-              <CardContent className="flex justify-between items-center">
-                <p className="text-sm font-semibold">Followers: {user.followers.length}</p>
-                <p className="text-sm font-semibold">Following: {user.following.length}</p>
-              </CardContent>
-              <CardFooter>
-                <Link href={`/${user.username}`} className={cn(buttonVariants({variant: "secondary"}), "w-full")}>
-                  Visit Profile
-                </Link>
-              </CardFooter>
-            </Card>
-          ))
-        }
-      </div>
+    <ResponsiveContainer>   
+      {
+        isPending ? (
+          <ResponsiveContainer className="flex flex-col items-center justify-center gap-2">
+            <LoaderCircle className="animate-spin text-white h-16 w-16"/>
+          </ResponsiveContainer>
+        ) : isError ? (
+          <ResponsiveContainer className="flex flex-col items-center justify-center gap-8">
+            <h1 className="md:text-4xl text-2xl font-bold text-white">No users found.</h1>
+            <Link href={"/"} className={buttonVariants({variant: "secondary"})}>
+              Return Home
+            </Link>
+          </ResponsiveContainer>
+        ) : (
+          <div className="grid w-full gap-4 sm:grid-cols-2 grid-cols-1">
+            {
+              data.map((user) => (
+                <Card key={user.id}>
+                  <CardHeader className="space-y-4">
+                    <Avatar>
+                      <AvatarImage src={user.image ? user.image : "/avatar.png"} />
+                      <AvatarFallback>{user.username?.charAt(0).toUpperCase()}</AvatarFallback>
+                    </Avatar>
+                    <CardTitle>{user.username}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex justify-between items-center">
+                    <p className="text-sm font-semibold">Followers: {user.followers.length}</p>
+                    <p className="text-sm font-semibold">Following: {user.following.length}</p>
+                  </CardContent>
+                  <CardFooter>
+                    <Link href={`/${user.username}`} className={cn(buttonVariants({variant: "secondary"}), "w-full")}>
+                      Visit Profile
+                    </Link>
+                  </CardFooter>
+                </Card>
+              ))
+            }
+          </div>
+        )
+    }
     </ResponsiveContainer>
   )
 }
