88a30dac83e9760e780b8c143407ed62a5979b77
Justin Ferber <119906373+j-ferber@users.noreply.github.com>
2024-07-12 19:03:10 -0400

    Added ability for user to modify their own post captions (#8)

---
+++ src/components/EditPostForm.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import React from 'react'
+import { api, RouterOutputs } from '~/trpc/react'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from './ui/form'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { Label } from './ui/label'
+import { useRouter } from 'next/navigation'
+import { useToast } from './ui/use-toast'
+
+type PostDataOutput = RouterOutputs['post']['getPostData']
+
+const editPostSchema = z.object({
+  caption: z.string().max(500, {message: 'Caption must be less than 500 characters'})
+})
+
+const EditPostForm = ({post} : {post: PostDataOutput}) => {
+
+  const editPostForm = useForm<z.infer<typeof editPostSchema>>({
+    resolver: zodResolver(editPostSchema),
+    defaultValues: {
+      caption: post.caption
+    }
+  })  
+
+  const router = useRouter()
+  const {toast} = useToast()
+
+  const onSubmit = (values: z.infer<typeof editPostSchema>) => {
+    mutate({postId: post.id, caption: values.caption})
+  }
+
+  const {mutate} = api.post.updatePost.useMutation({
+    onSuccess: async () => {
+      router.push(`/p/${post.id}`)
+      toast({
+        title: 'Post Updated',
+        description: 'Your post has been updated successfully'
+      })
+    },
+    onError: error => {
+      toast({
+        title: 'Post Error',
+        description: error.message,
+        variant: 'destructive'
+      })
+    }
+  })
+  
+  return (
+    <Form {...editPostForm}>
+      <form onSubmit={editPostForm.handleSubmit(onSubmit)} className='space-y-8 w-full'>
+        <FormField
+          control={editPostForm.control}
+          name='caption'
+          render={({field}) => (
+            <FormItem>
+              <Label>Caption</Label>
+              <FormControl>
+                <Input placeholder='Caption...' {...field} />
+              </FormControl>
+              <FormDescription>
+                Add/Edit your caption here.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit' variant={"secondary"}>Save</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default EditPostForm
